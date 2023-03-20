c45c02380da42b61b96dd304818915d5b9370422
evgenii klimashko <klimashko108@gmail.com>
2023-03-20 10:34:34 +0300

    Добавил к проекту файлы conftest, requirements,__init__, изменил .gitignore. Добавил файл csv

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,10 @@ import csv
 
 @pytest.fixture()
 def download_csv():
-    with open('C:/Users/klima/PycharmProjects/qa_guru_python_4_7_about_files/tests/addresses.csv') as f:
+    with open('/tests/addresses_1.csv') as f:
         reader = csv.reader(f)
         for row in reader:
             print(row)
+
+
+            #os.getcwd()+'/picture1.jpg'
--- tests/test_file_actions.py
+++ tests/test_file_actions.py
@@ -3,12 +3,14 @@
 #https://axiomabio.com/pdf/test.pdf
 import pytest
 from selene import browser
+import os
 import csv
 
 #https://stats.govt.nz/assets/Uploads/Business-operations-survey/Business-operations-survey-2021/Download-data/bos2021ModC.csv
 
 def test_download_csv():
-    with open('C:/Users/klima/PycharmProjects/qa_guru_python_4_7_about_files/tests/addresses.csv') as f:
+    #with open('C:/Users/klima/PycharmProjects/qa_guru_python_4_7_about_files/tests/addresses_1.csv') as f:
+    with open(os.getcwd()+'/addresses_1.csv') as f:
         reader = csv.reader(f)
         for row in reader:
             print(row)
